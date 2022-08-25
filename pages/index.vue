5fca2a3946c7492f33445a5ef92ff2375930b6f4
Rotceh Dominguez <rotceh.dominguez@zaelot.com>
2022-08-25 08:46:48 -0400

    vuelidate + vuejs 2.7.10

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  <div>
+    <label>Field:</label>
+    <input v-model="state.firstName">
+    <div v-if="v$.firstName?.$error">firstName field has an error.</div>
+  </div>
+  State: 
+  <br>
+  <pre>
+    {{state}}
+  </pre>
+  Vuelidate:
+  <br>
+  <pre>
+    {{v$}}
+  </pre>
+</div>
+</template>
+
+<script>
+import { reactive } from 'vue' // "from '@vue/composition-api'" if you are using Vue 2.x
+import useVuelidate from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+
+export default {
+  setup () {
+    const state = reactive({
+      firstName: ''
+    })
+    const rules = {
+      firstName: { required }, // Matches state.firstName
+    }
+
+    const v$ = useVuelidate(rules, state)
+
+    return { state, v$ }
+  }
+}
+</script>
+</script>
